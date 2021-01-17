c4b5518a9b5f1341ea8325bea65ac59d0d04de57
Valerie <akpotevalerie@gmail.com>
2021-01-17 15:36:37 +0100

    Add form

--- Starter-packs/1.Guessing-game/view.php
+++ Starter-packs/1.Guessing-game/view.php
@@ -8,5 +8,20 @@
 	<title>Casino royale - guessing game</title>
 </head>
 <body>
+	<div id="game-container">
+		<header>
+			<h1>GUESSING GAME</h1>
+		</header>
+		<main>
+			<p>Can you guess the secret number between 1 and 22?</p>
+			<form action="" method="POST">
+				<input type="number" id="guess" min=1 max=22 name="guess" placeholder="Your guess:" 
+                required autocomplete="off"
+                oninvalid="this.setCustomValidity('Fill in a number between 1 and 22!')" 
+                oninput="setCustomValidity('')"><br><br>
+                <input type="submit" id="button" value="PLAY NOW">
+			</form>
+		</main>
+	</div>
 </body>
 </html>
