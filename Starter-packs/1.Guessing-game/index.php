b044069e13eb82b502f8ebccf57332d9e7635eb0
Valerie <akpotevalerie@gmail.com>
2021-01-19 00:00:32 +0100

    Add whatIsHappening function

--- Starter-packs/1.Guessing-game/index.php
+++ Starter-packs/1.Guessing-game/index.php
@@ -14,6 +14,18 @@ session_start();
 // Load you classes
 require_once 'classes/GuessingGame.php';
 
+function whatIsHappening() {
+    echo '<h2>$_GET</h2>';
+    var_dump($_GET);
+    echo '<h2>$_POST</h2>';
+    var_dump($_POST);
+    echo '<h2>$_COOKIE</h2>';
+    var_dump($_COOKIE);
+    echo '<h2>$_SESSION</h2>';
+    var_dump($_SESSION);
+}
+// whatIsHappening();
+
 // Function to hide form when user submits the amount of times he/she would like to guess
 function hideForm() {
     if (isset($_POST["maxGuesses-bttn"])) {
