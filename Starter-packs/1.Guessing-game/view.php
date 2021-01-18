58f4cd6530db7c1cb14a961962de14fe18004caf
Valerie <akpotevalerie@gmail.com>
2021-01-18 01:44:20 +0100

    Add form max guesses

--- Starter-packs/1.Guessing-game/view.php
+++ Starter-packs/1.Guessing-game/view.php
@@ -8,6 +8,20 @@
 	<title>Casino royale - guessing game</title>
 </head>
 <body>
+<div id="maxGuess-container">
+		<header>
+			<h1>GUESSING GAME</h1>
+		</header>
+		<main>
+			<p>How many times would you like to guess?</p>
+			<form action="" method="POST">
+				<input type="text" id="maxGuess" name="maxGuess" required autocomplete="off"
+                oninvalid="this.setCustomValidity('Please, this is required to start the game!')" 
+                oninput="setCustomValidity('')"><br><br>
+                <input type="submit" class="button" value="START THE GAME">
+			</form>
+		</main>
+	</div>
 	<div id="game-container">
 		<header>
 			<h1>GUESSING GAME</h1>
@@ -20,13 +34,16 @@
                 oninvalid="this.setCustomValidity('Fill in a number between 1 and 22!')" 
                 oninput="setCustomValidity('')"><br><br>
                 <input type="submit" class="button" value="PLAY NOW"><br><br>
-				<input type="submit" class="button" value="RESET">
+				<input type="submit" class="button" value="RESET GAME">
 			</form>
 		</main>
 	</div>
 	<style>
 		@import url('https://fonts.googleapis.com/css2?family=Secular+One&display=swap');
 		@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+		* {
+			box-sizing: border-box;
+		}
 
 		body {
 			display: flex;
@@ -38,10 +55,15 @@
 			background-color: #bbbfca;
 		}
 
-		#game-container {
+		#game-container, #maxGuess-container {
 			background-color: #525d6d;
 			border-style: solid;
 			padding: 40px;
+			width: 450px;
+		}
+
+		#maxGuess-container {
+			background-color: #eb596e;
 		}
 
 		h1 {
@@ -52,6 +74,10 @@
 
 		p {
 			margin-top: 0px;
+		}
+
+		#maxGuess {
+			width: 50px;
 		}
 
 		#guess {
