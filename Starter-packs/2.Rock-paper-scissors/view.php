93982443ed0b1027b3f209462d43e781452e83fb
Valerie <akpotevalerie@gmail.com>
2021-01-19 14:37:02 +0100

    Add stylesheet

--- Starter-packs/2.Rock-paper-scissors/view.php
+++ Starter-packs/2.Rock-paper-scissors/view.php
@@ -5,21 +5,35 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="style.css">
 	<title>Casino royale - rock, paper, scissors</title>
 </head>
 <body>
+	<header>
+		<h1>ROCK PAPER SCISSORS</h1>
+	</header>
+	<main>
+		<div class="result-container">
+			<div class="player">
+				<p>YOU</p>
+				<img src="../../images/start-left.PNG" alt="rock">
+			</div>
+			<div class="result">
+				<p>LET's PLAY!</p>
+			</div>
+			<div class="opponent">
+				<p>OPPONENT</p>
+				<img src="../../images/start-right.PNG" alt="rock">
+			</div>
+		</div>
+		<div class="RPS-container">
+			<button type="submit" name="rock">ROCK</button>
+			<button type="submit" name="paper">PAPER</button>
+			<button type="submit" name="scissors">SCISSORS</button>
+		</div>
+		<div class="reset-container">
+			<button type="submit" name="reset">PLAY AGAIN</button>
+		</div>
+	</main>
 </body>
-<style>
-	* {
-		box-sizing: border-box;
-		margin: 0px;
-		padding: 0px;
-	}
-
-	body {
-		background-color: #1B48C5;
-		height: 100vh;
-		width: 100vw;
-	}
-</style>
 </html>
